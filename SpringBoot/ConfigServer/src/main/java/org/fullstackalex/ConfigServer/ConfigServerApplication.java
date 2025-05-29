26b2efe2aa6acb090345a7c1804ed2e8e4f5661c
alex_medero <alexmedero10@gmail.com>
2025-05-29 16:56:38 -0600

    Added eureka

--- SpringBoot/ConfigServer/src/main/java/org/fullstackalex/ConfigServer/ConfigServerApplication.java
+++ SpringBoot/ConfigServer/src/main/java/org/fullstackalex/ConfigServer/ConfigServerApplication.java
@@ -1,8 +1,13 @@
 package org.fullstackalex.ConfigServer;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
