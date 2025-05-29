ed9a3586762fa3bf5fe732f8469d23b1ea05e261
alex_medero <alexmedero10@gmail.com>
2025-05-29 15:29:40 -0600

    Congig Server for E-Commerce

---
+++ SpringBoot/ConfigServer/src/main/java/org/fullstackalex/ConfigServer/ConfigServerApplication.java
@@ -0,0 +1,8 @@
+package org.fullstackalex.ConfigServer;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
